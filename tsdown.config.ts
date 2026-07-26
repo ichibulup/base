e61ab880bd59270a7c7bbb6efa122bb9e04aa4e6
Truong Giang Pham <gorariagroup@gmail.com>
2026-07-27 02:45:34 +0700

    refactor: change package bundle from tsup to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,41 @@
+/// <reference types="node" />
+
+import { defineConfig } from "tsdown"
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  bundle: true,
+  // Keep shared modules (e.g. React contexts) in common chunks
+  // so different entrypoints consume the same runtime instance.
+  // splitting: true,
+  entry: [
+    "src/index.ts",
+    "src/components/**/*.tsx",
+    "src/layouts/*.tsx",
+    "src/providers/*.tsx",
+    "src/hooks/*.ts",
+    "src/hooks/*.tsx",
+    "src/lib/**/*.ts",
+    "src/lib/**/*.tsx",
+    "src/cores/**/*.ts",
+    "src/modules/*.ts",
+    "src/assets/*.png",
+    "src/assets/*.jpg",
+    "src/assets/*.jpeg",
+  ],
+  loader: {
+    '.png': 'copy',
+    '.jpg': 'copy',
+    '.jpeg': 'copy',
+    '.svg': 'copy',
+  },
+  format: ["esm"],
+  sourcemap: false,
+  minify: false,
+  target: "es2024",
+  outDir: "dist",
+  treeshake: true,
+
+  external: ["react", "react-dom"],
+})
